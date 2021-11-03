9c874097d15dc387067af8d9a23fbe5c652ffd70
thomas-nivot <thomas_nivot@hotmail.fr>
2021-11-03 01:57:34 +0100

    reproduction of the model

---
+++ components/FaqItem.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <div v-bind:class="{grey:isClicked, lightgrey: !isClicked}" class="mb-5 py-4 md:py-8 px-5 md:px-10 flex transition-colors duration-300" >
+        <div class="w-full">
+            <p v-on:click="click" v-bind:class="{'mb-6': isClicked}" class="md:text-xl font-semibold flex justify-between cursor-pointer transition-all duration-300">
+                <span class="my-auto">{{title}}</span>
+                <span  class="my-auto">
+                    <svg
+                        v-bind:class="{'rotate-180': isClicked, 'rotate-0': !isClicked }"
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 12 6" 
+                        class="transform transition duration-300 w-6 text-gray-a6 stroke-current items-center"
+                    >
+                        <path fill="none" stroke-linecap="round" d="M1 .5l5 5 5-5" stroke="currentColor"></path>
+                    </svg>
+                    
+                </span>
+            </p>
+
+            <div v-bind:style="{height: !isClicked ? '0' : '46px'}" class="md:border-l-8 md:pl-6 border-grey text-sm transition-all duration-300 overflow-hidden">
+                <p class="font-medium mb-1">{{ subTitle}}</p>
+                <p>{{content}}</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'FaqItem',
+    props:["title","subTitle" ,"content", "index"],
+    data(){
+        return {isClicked: this.$props.index===0}
+    },
+
+    methods:{
+        click: function(){
+            this.isClicked=!this.isClicked
+        }
+    }
+};
+</script>
+
+<style>
+    .grey{
+        background-color: #e9eaf2;
+    }
+    .lightgrey{
+        background-color: #f6f6f6;
+    }
+    .border-grey{
+        border-left: 8px solid #e0e1ec;
+    }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  <section class="mx-28 mt-24" >
+    <FaqItem 
+      v-for="(item,index) in data" 
+      :key="item.title" 
+      v-bind:index="index"
+      v-bind:title="item.title" 
+      v-bind:subTitle="item.subtitle" 
+      v-bind:content="item.content"
+    />
+  </section>
+
+  <section class="mx-28 mt-24 md:my-24">
+    <h3 class="text-xl md:text-4xl font-semibold mb-8">Vous n’avez pas trouvé de réponse à votre question ? Contactez-nous !</h3>
+    <div class="flex flex-col md:flex-row md:flex-wrap">
+      <div class="flex  mb-4 md:mr-24 last:mr-0"><div class="mr-6 my-auto"><a href="tel:02 37 64 84 40" class="md:hidden block"><img src="https://admin.shoppingauto.fr/storage/uploads/2020/12/15/5fd88680e42195d7fae39ec200PICTO_Hotline.svg" class="w-16"></a> <button class="hidden md:block"><img src="https://admin.shoppingauto.fr/storage/uploads/2020/12/15/5fd88680e42195d7fae39ec200PICTO_Hotline.svg" class="w-16"></button></div> <div class="my-auto flex-1"><p class="text-base md:text-xl font-semibold">
+          Hotline téléphonique
+        </p> 
+      <div class="text-sm"><p>Du lundi au samedi de 9h00 à 19h00 au <a href="tel:0237648440">02 37 64 84 40</a>.</p></div></div></div><div class="flex mb-4 md:mr-24 last:mr-0"><img src="https://admin.shoppingauto.fr/storage/uploads/2020/12/15/5fd886839aec65d7fae3a008b3PICTO_Chat.svg" class="w-16 my-auto mr-6"> <div class="my-auto flex-1"><p class="text-base md:text-xl font-semibold">
+          Chat en ligne
+        </p> <div class="text-sm">7 jours sur 7, de 6h à minuit</div></div></div>
+    </div>
+  </section>
+</div>
+</template>
+
+<script>
+
+import FaqItem from '~/components/FaqItem';
+
+export default {
+
+  components: { FaqItem },
+
+  data(){
+    const subtitle = "La réponse de shoppingauto.fr :"
+    const data = [
+      {title:"shoppingauto.fr, comment ça marche ?", subtitle, content:"Acheter une auto n'aura jamais été aussi facile. Cherchez, comparez et réservez ! Dès réception de votre réservation, on se charge de finaliser votre commande à vos côtés et à 100% en digital ! 😊"},
+      {title:"J'aimerai avoir des infos complémentaires sur un véhicule ?", subtitle, content:"Une question sur la finition ?  Les options ? Une trace d'usure ? Nos conseillers shoppingauto.fr vous répondent directement sur notre chat ! 😎"},
+      {title:"Comment savoir combien coutera ma livraison ?", subtitle, content:'Rien de plus simple : Une fois votre véhicule choisie, appuyez sur le bouton "je shop" et une étape plus tard on vous demandera dans quel département vous souhaitait être livré et on vous indiquera le tarif ! 🚀'},
+      {title:"Comment réserver le véhicule ?", subtitle, content:"Afin de bloquer la voiture de vos rêves, nous vous invitons à réserver le véhicule en ligne en versant un acompte de 250€, un bon de réservation vous sera transmis automatiquement par mail. "},
+      {title:"Comment payer le solde du véhicule ?", subtitle, content:"Le montant de l'acompte sera enlevé du montant global et notre service administratif se chargera de vous envoyer la facture, le bon de commande et notre RIB. Pour le paiement du solde, un virement sera exigé en amont de la livraison et dès reception des fonds, la livraison pourra être effectuée."},
+      {title:"Que faire si le véhicule ne me convient pas ?", subtitle, content:"Pas de panique ! Vous êtes protégé avec notre garantie satisfait ou remboursé."}
+    ]
+    return {data};
+  }
+
+};
+
+</script>
